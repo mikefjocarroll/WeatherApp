de686dbb452a7e8991e464783aaf383b3f8e0e67
O'Carroll <mocarroll@doverfoodretail.com>
2025-07-19 08:58:35 -0500

    building WeatherAPIService

---
+++ WeatherApp.Logic/WeatherAPIService.cs
@@ -0,0 +1,14 @@
+﻿namespace WeatherApp.Logic;
+
+public class WeatherAPIService
+{
+    public double GetFahrenheit(double kelvin)
+    {
+        return (kelvin - 273.15) * 9 / 5 + 32;
+    }
+
+    public double GetCelsius(double kelvin)
+    {
+        return kelvin - 273.15;
+    }
+}
